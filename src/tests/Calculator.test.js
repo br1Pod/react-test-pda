1e1f202ee79a2a45845c1a86a73bc9db457d0fd8
boabsy <boabsy@penguin.lxd>
2021-08-31 16:57:52 +0100

    first test written. need to fix install before can test.. in progress

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Calculator from '../containers/Calculator';
 import {mount, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { eq } from 'cypress/types/lodash';
 
 configure({adapter: new Adapter()});
 
@@ -16,6 +17,20 @@ describe('Calculator', () => {
     const runningTotal = container.find('#running-total');
     button4.simulate('click');
     expect(runningTotal.text()).toEqual('4');
+  })
+
+  // calculator.add() - add 1 to 4 and get 5
+  it('should return correct answer for addition', () => {
+    const button1 = container.find('#number1');
+    button1.simulate('click');
+    const addition = container.find('operator_add');
+    addition.simulate('click')
+    const button4 = container.find('#number4');
+    button4.simulate('click');
+    const equals = container.find('operator-equals');
+    equals.simulate('click');
+    const runningTotal = container.find('#running-total')
+    expect(runningTotal.text()).toEqual('5');
   })
 })
 
