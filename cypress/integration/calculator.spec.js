808fe1c04c948b62f007897ed0c86268f7eb5644
briPod <33463220+br1Pod@users.noreply.github.com>
2021-09-01 01:11:32 +0100

    Cypress test1 - update display - Pass

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,24 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  // Do the number buttons update the display of the running total?
+  it('running total should display result of a sum', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain','20')
+  })
+
+  // Do the arithmetical operations update the display with the result of the operation?
+  
+  // Can multiple operations be chained together?
+  // Is the output as expected for a range of numbers?
+  // Does divide by 0 return 0?
+
+
+
+
+
 })
